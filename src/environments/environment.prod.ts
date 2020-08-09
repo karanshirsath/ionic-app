6fc9442a8c5ccae1367b5ebd5f079cf2043b0132
KARAN <karan@KARANs-MacBook-Air.local>
2020-08-09 21:27:07 +0530

    second

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAYmkEzn7WzZxuTr_zZ8vehY1BWKYGEQUQ",
+    authDomain: "userionic-412d7.firebaseapp.com",
+    databaseURL: "https://userionic-412d7.firebaseio.com",
+    projectId: "userionic-412d7",
+    storageBucket: "userionic-412d7.appspot.com",
+    messagingSenderId: "708491116831",
+    appId: "1:708491116831:web:706681f0e808c1dec4302a",
+    measurementId: "G-RV236DD885"
+  }
 };
